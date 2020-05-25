ca3a36db0b87f8c6413e174624281deb38966a63
imaginedragons <anson.mec@gmail.com>
2020-05-25 19:35:57 +0530

    first commit

---
+++ djb2/djb2.py
@@ -0,0 +1,19 @@
+
+import re
+
+
+def djb2(str):
+
+    if(type(str) == list):
+        print(algo("".join(str)))
+    else:
+        print(algo(str))
+
+
+def algo(val):
+    hash = 5381
+
+    for st in val:
+        hash = (((hash << 5) + hash) + ord(st))
+
+    return hash & 0xffffffffffffffff  # return 64 bit version
